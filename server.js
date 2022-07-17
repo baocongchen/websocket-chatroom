f15ab082ebc9a16ce5bd1fe1f66aaecb92c42f49
baocongchen <peter_tran_cm@yahoo.com>
2022-07-16 20:31:18 -0400

    finshed websocket chat app

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 ///////////// IMPORTS + VARIABLES /////////////
 ///////////////////////////////////////////////
 
-const http = require('http'); 
-const CONSTANTS = require('./utils/constants.js');
-const fs = require('fs');
-const path = require('path');
-const WebSocket = require('ws');
+const http = require("http");
+const CONSTANTS = require("./utils/constants.js");
+const fs = require("fs");
+const path = require("path");
+const WebSocket = require("ws");
+const { connected } = require("process");
 
-// You may choose to use the constants defined in the file below
 const { PORT, CLIENT } = CONSTANTS;
 
 ///////////////////////////////////////////////
@@ -16,35 +16,38 @@ const { PORT, CLIENT } = CONSTANTS;
 ///////////////////////////////////////////////
 
 // Create the HTTP server
-const server = http.createServer((req, res) => {
+const httpServer = http.createServer((req, res) => {
   // get the file path from req.url, or '/public/index.html' if req.url is '/'
-  const filePath = ( req.url === '/' ) ? '/public/index.html' : req.url;
+  const filePath = req.url === "/" ? "/public/index.html" : req.url;
 
   // determine the contentType by the file extension
   const extname = path.extname(filePath);
-  let contentType = 'text/html';
-  if (extname === '.js') contentType = 'text/javascript';
-  else if (extname === '.css') contentType = 'text/css';
+  let contentType = "text/html";
+  if (extname === ".js") contentType = "text/javascript";
+  else if (extname === ".css") contentType = "text/css";
 
   // pipe the proper file to the res object
-  res.writeHead(200, { 'Content-Type': contentType });
-  fs.createReadStream(`${__dirname}/${filePath}`, 'utf8').pipe(res);
+  res.writeHead(200, { "Content-Type": contentType });
+  fs.createReadStream(`${__dirname}/${filePath}`, "utf8").pipe(res);
 });
 
 ///////////////////////////////////////////////
 ////////////////// WS LOGIC ///////////////////
 ///////////////////////////////////////////////
 
-// TODO
-// Exercise 3: Create the WebSocket Server using the HTTP server
+// Create the WebSocket Server using the HTTP server
+const wsServer = new WebSocket.Server({ server: httpServer });
 
+// Respond to connection events
+wsServer.on("connection", (socket) => {
+  console.log("A new client has connected to the server!");
 
-// TODO
-// Exercise 5: Respond to connection events 
-  // Exercise 6: Respond to client messages
-  // Exercise 7: Send a message back to the client, echoing the message received
-  // Exercise 8: Broadcast messages received to all other clients
-  
+  socket.on("message", (data) => {
+    console.log(data);
+    // Broadcast messages received to all other clients
+    broadcast(data, socket);
+  });
+});
 
 ///////////////////////////////////////////////
 ////////////// HELPER FUNCTIONS ///////////////
@@ -53,10 +56,17 @@ const server = http.createServer((req, res) => {
 function broadcast(data, socketToOmit) {
   // TODO
   // Exercise 8: Implement the broadcast pattern. Exclude the emitting socket!
+  wsServer.clients.forEach((connectedClient) => {
+    if (
+      connectedClient.readyState === WebSocket.OPEN &&
+      connectedClient !== socketToOmit
+    ) {
+      connectedClient.send(data);
+    }
+  });
 }
 
 // Start the server listening on localhost:8080
-server.listen(PORT, () => {
-  console.log(`Listening on: http://localhost:${server.address().port}`);
+httpServer.listen(PORT, () => {
+  console.log(`Listening on: http://localhost:${wsServer.address().port}`);
 });
-
